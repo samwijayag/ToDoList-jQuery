e9fd7fec35e2112d420907474e099e5983c82070
samwijayag <sam.wijayag@gmail.com>
2019-03-17 23:08:35 -0700

    final commit

---
+++ script.js
@@ -0,0 +1,29 @@
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("completed");
+});
+
+//click on X to delete todo
+$("ul").on("click", "span", function(event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
+    });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    //grabbing new to do from input
+    var todoText = $(this).val();
+    $(this).val("");
+    //create a new li and add to Ul
+    $("ul").append(
+      "<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>"
+    );
+  }
+});
+
+$('.fa-pencil').click(function(){
+  $("input[type='text']").fadeToggle()
+});
